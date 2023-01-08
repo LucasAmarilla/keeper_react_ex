ea7647991a0eb14b98f3d24acf25aa6854e4b97c
LucasAmarilla <amarilla482@gmail.com>
2023-01-08 10:17:06 -0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer>
+        <p>Feito por LucasP1ka2</p>
+      </footer>
+    );
+  }
+  
---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>Titulo</h1>
+      <p>Conteudo </p>
+    </div>
+  );
+}
