33606d717fcf9bf2eed359d31affcfe93016fd47
Lucas Amarilla <63009565+LucasAmarilla@users.noreply.github.com>
2023-01-15 12:46:58 -0400

    array_added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Note from './components/Note';
+import notes from './notes';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
-      <Footer/>
+      {notes.map((noteItem) => {
+        return <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      })}
+      <Footer />
     </div>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,8 @@
-export default function Note() {
+export default function Note(props) {
   return (
     <div className="note">
-      <h1>Titulo</h1>
-      <p>Conteudo </p>
+      <h1> {props.title} </h1>
+      <p> {props.content} </p>
     </div>
   );
 }
